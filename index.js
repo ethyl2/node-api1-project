e95bab277cfd65b42b60a19be197edc4f8683c30
Heather Nuffer <zzzuzz@gmail.com>
2019-12-31 21:10:07 -0700

    Set up server to listen at port 8000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('Welcome to Hobbiton');
+});
+
+const port = 8000;
+server.listen(port, () => console.log(`API up and running on port ${port}`));
