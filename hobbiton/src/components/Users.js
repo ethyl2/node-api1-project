84d946aedca883972d7f5226fb3730a898700a40
Heather Nuffer <zzzuzz@gmail.com>
2020-01-01 00:24:46 -0700

    Added middleware to allow cross domain

--- hobbiton/src/components/Users.js
+++ hobbiton/src/components/Users.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
     //tried using https://cors-anywhere.herokuapp.com/ but no success
     useEffect(() => {
         axios.get('localhost:3000/api/users')
