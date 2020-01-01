b9ea9420ab29326c58128d653a8f1a9306a577d2
Heather Nuffer <zzzuzz@gmail.com>
2019-12-31 21:15:56 -0700

    Client can GET all users

--- index.js
+++ index.js
@@ -7,5 +7,29 @@ server.get('/', (req, res) => {
     res.send('Welcome to Hobbiton');
 });
 
+/* 
+When the client makes a GET request to /api/users:
+
+    If there's an error in retrieving the users from the database:
+        respond with HTTP status code 500.
+        return the following JSON object: { errorMessage: "The users information could not be retrieved." }.
+
+*/
+server.get('/api/users', (req, res) => {
+    console.log('Requesting users');
+    db.find()
+        .then(users => {
+            console.log(users);
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "The users' information could not be retrieved." });
+        });
+})
+
+
+
+
 const port = 8000;
 server.listen(port, () => console.log(`API up and running on port ${port}`));
