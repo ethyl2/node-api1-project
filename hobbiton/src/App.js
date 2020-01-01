93c047edcbc745e0c654ecfcc7f32489ad2d416f
Heather Nuffer <zzzuzz@gmail.com>
2019-12-31 23:51:03 -0700

    Added React app

---
+++ hobbiton/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Users from './components/Users';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to Hobbiton</h1>
+      </header>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ hobbiton/src/components/Users.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Users = () => {
+    //tried using https://cors-anywhere.herokuapp.com/ but no success
+    useEffect(() => {
+        axios.get('localhost:3000/api/users')
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, []);
+    
+    return (
+        <div>Users AKA Hobbits go here</div>
+    );
+}
+
+export default Users;
