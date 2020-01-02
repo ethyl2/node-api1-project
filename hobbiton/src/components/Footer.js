f4178308b1e2ce1075b2ab90c73a8e2315026880
Heather Nuffer <zzzuzz@gmail.com>
2020-01-01 22:12:54 -0700

    Added sound sources to footer

--- hobbiton/src/components/Footer.js
+++ hobbiton/src/components/Footer.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Footer = () => {
+    const [seeSoundSources, setSeeSoundSources] = useState(false);
+    const showSoundSources = () => {
+        setSeeSoundSources(!seeSoundSources);
+    }
+
     return (
         <footer>
+            
+            {!seeSoundSources && <button onClick={showSoundSources}>Sound Sources</button>}
+            {seeSoundSources && <div className='soundSources'>
+                <a href='https://freesound.org/s/445802/'>Knob</a>
+                <a href='https://freesound.org/s/395830/'>Birds</a>
+                <a href='https://freesound.org/s/341738/'>Goodbye</a>
+                <a href='https://freesound.org/s/449069/'>Fanfare</a>
+                <a href='https://freesound.org/s/343898/'>Hello</a>
+            </div>}
             <p>Copyright 2020 Heather Nuffer</p>
         </footer>
     )
